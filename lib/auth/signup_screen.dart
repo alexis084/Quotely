25afc877be5f3951bd4493a4fb906b4c56b596c5
alexis084 <64110995+alexis084@users.noreply.github.com>
2021-05-20 21:48:11 -0600

    Refactor of auth

---
+++ lib/auth/signup_screen.dart
@@ -0,0 +1,71 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  String email = '';
+  String password = '';
+
+  void signUp() async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        elevation: 8,
+        title: Text('Sign Up'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 50,
+        ),
+        child: new Form(
+          child: Column(
+            children: [
+              TextFormField(
+                onChanged: (val) => setState(() {
+                  email = val;
+                }),
+              ),
+              TextFormField(
+                onChanged: (val) => setState(() {
+                  password = val;
+                }),
+              ),
+              ElevatedButton(
+                child: Text('Sign Up'),
+                onPressed: () async => {
+                  signUp(),
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:twitter_app/auth_service.dart';
+import 'package:twitter_app/auth/signup_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: AuthService().handleAuth(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Text('404 error has occured');
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primaryColor: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            home: SignUp(),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Text('Loading...');
+      },
     );
   }
 }
